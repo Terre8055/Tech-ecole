a75479211725564430bcca864c13152c43fe149c
Terre8055 <michaelappiah2018@icloud.com>
2023-01-24 21:02:56 +0000

    Added a logo

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -7,8 +7,9 @@ export default function Navbar(){
         <section className="header">
             <nav>
                 <a href="index.html">
-                    <img src="" alt="logo" />
+                    <img src="./logo.png" alt="logo" />
                 </a>
+                {/* <h3 className="nav--title">Tech-eCole</h3> */}
                 <input type="text" placeholder="Search for resources"/>
                 <button className="hero-btn">SEARCH</button>
                 <div className="nav--links">
